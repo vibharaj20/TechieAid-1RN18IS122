5a5bafdcc8f1f52b48398892dd753d40e971d33a
vibharaj20 <63732535+vibharaj20@users.noreply.github.com>
2021-12-06 19:58:03 +0530

    Add files via upload

---
+++ usercontroller.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+public ActionResult Registration()
+{
+    return View();
+}
